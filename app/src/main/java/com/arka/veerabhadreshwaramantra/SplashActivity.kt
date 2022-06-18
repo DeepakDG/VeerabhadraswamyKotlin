b7121bb30835a3f92a8f4e1240ae4b785698115c
DeepakWin10 <deepakganachari3@gmail.com>
2022-06-18 13:16:42 +0530

    splash screen added

---
+++ app/src/main/java/com/arka/veerabhadreshwaramantra/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.arka.veerabhadreshwaramantra
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+        // This is the loading time of the splash screen
+        private val splashTimeOut:Long = 3000 // 1 sec
+        @Suppress("DEPRECATION")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.splashscreen)
+
+            Handler().postDelayed({
+                // This method will be executed once the timer is over
+                // Start your app main activity
+
+                startActivity(Intent(this,MainActivity::class.java))
+                // close this activity
+                finish()
+            }, splashTimeOut)
+
+    }
+}
