777fe437881bfe84d75b4ed53233def13a56072b
DeepakWin10 <deepakganachari3@gmail.com>
2022-07-24 00:20:30 +0530

    multi array list added for pos

--- app/src/main/java/com/arka/veerabhadreshwaramantra/ViewPagerDashboard.kt
+++ app/src/main/java/com/arka/veerabhadreshwaramantra/ViewPagerDashboard.kt
@@ -11,17 +11,153 @@ class ViewPagerDashboard : AppCompatActivity() {
     private lateinit var viewPager2: ViewPager2
     private lateinit var btnPrevious: Button
     private lateinit var btnNext: Button
+    private var imagesList = listOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.viewpager_dashboard)
         val pos = intent.getIntExtra("Position", 0)
         Toast.makeText(applicationContext, "Rel" + pos, Toast.LENGTH_LONG).show()
-        val imagesList = listOf(
-            R.string.vadapu3,
-            R.string.vadapu3,
-            R.string.vadapu3
-        )
+        if (pos == 0) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 1) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 2) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 3) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 4) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 5) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 6) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 7) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 8) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 9) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 10) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 11) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 12) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 13) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 14) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 15) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 16) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 17) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 18) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 18) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 19) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 20) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        } else if (pos == 21) {
+             imagesList = listOf(
+                R.string.vadapu3,
+                R.string.vadapu3,
+                R.string.vadapu3
+            )
+        }
+
         btnPrevious = findViewById(R.id.btnPrevious)
         btnNext = findViewById(R.id.btnNext)
         val adapter = ViewPagerAdapter(imagesList)
